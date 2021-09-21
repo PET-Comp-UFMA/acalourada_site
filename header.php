eb847175793031c6005fe10eb842b06cefe2229b
Luigi Dovera <luigidovera11@gmail.com>
2021-09-21 08:38:51 -0300

    testando php em git io

--- header.php
+++ header.php
@@ -1,1 +1,74 @@
-<?php ?>
+<div class="wrapper">
+    <div class="banner">
+        <div>
+            <a href="index.html"><img src="img/favicon.ico" alt="" class="img-fluid" id="img-esq"></a>
+            <a href="http://www.deinf.ufma.br/cocom/site/"><img src="img/LOGOCocom2.png" alt="" class="img-fluid"
+                    id="img-esq-1"></a>
+        </div>
+        <div>
+            <a href="index.html"><img src="img/banner2021_1.png" class="img-fluid" id="img-centro"></a>
+        </div>
+        <div>
+            <a href="https://portalpadrao.ufma.br/site" target="_blank"><img src="img/supporters/1.png" alt=""
+                    class="img-fluid" id="img-dir"></a>
+        </div>
+    </div>
+
+    <header id="header">
+        <div class="container">
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li><a href="index.html">Home</a></li>
+                    <!--<li class="dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Chamada</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="trabalhos.html">Chamada de Trabalhos</a> 
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="competitiva.html">Mostra Competitiva</a>  
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="maratona.html">Maratona</a>
+                    </div>
+                    
+                    </a>
+                </li>-->
+                    <li class="menu-active"><a href="inscricoes.html">Inscrições</a></li>
+                    <li><a href="programacao.html">Programação</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown"
+                            role="button" aria-haspopup="true" aria-expanded="false">Informações</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="materiais.html">Materiais</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="faq.html">FAQ</a>
+                        </div>
+                        </a>
+                    </li>
+                    <!--<li class="dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pós-evento</a>   
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="anais.html">Anais</a> 
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="certificados.html">Certificados</a>     
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="deliberacoes.html">Deliberações</a>              
+                    </div>
+                    </a>
+                </li>-->
+                    <li class="dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown"
+                            role="button" aria-haspopup="true" aria-expanded="false">Versões</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="index.html">2021.1</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="2020.2/index.html">2020.2</a>
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- #nav-menu-container -->
+
+        </div>
+    </header>
