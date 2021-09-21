972a941d47f32a1374b8af0ae9e64e62c706c3e8
Luigi Dovera <luigidovera11@gmail.com>
2021-09-21 09:12:52 -0300

    tirei páginas não usadas e modularização de header

---
+++ inscricoes.php
@@ -0,0 +1,56 @@
+<?php
+  require_once('html_header.php');
+  require_once('header.php');
+?>
+        <main id="main">
+
+            <section id=inscricao>
+                <div class="container">
+                    <div class="section-header">
+                        <h2>INSCRIÇÕES</h2>
+                    </div>
+
+                    <div class="text-center">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-9">
+
+                                <!--<p>Orientações para participação da Acalourada Digital</p>-->
+
+                                <ol>
+                                    <ol>
+                                        <strong>
+                                            Inscreva-se no <a href="https://forms.gle/vMCmDHgovw3SeTqj8">Formulário de
+                                                Inscrição</a> a para participar do evento.
+                                        </strong>
+                                    </ol>
+                                    <ol>
+                                        <strong>
+                                            Acesse o grupo do <a
+                                                href="https://chat.whatsapp.com/ILPySnxXCLF8RQhJte9K9e">Whatsapp</a>
+                                            para ficar por dentro de tudo ou fale conosco pelo email.
+                                        </strong>
+                                    </ol>
+                                    <ol>
+                                        <strong>
+                                            A sessão de jogos será realizada através do <a
+                                                href="https://discord.gg/jgUJ5Bq5gK">Discord</a> da Acalourada.
+                                        </strong>
+                                    </ol>
+                                </ol>
+                            </div>
+                        </div>
+
+                        <br>
+                        <button class="faq-whatsapp btn"><a class="btn-subscription-text"
+                                href="https://forms.gle/vMCmDHgovw3SeTqj8">Inscreva-se</a></button>
+                        <br><br>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+<?php 
+    require_once('footer.php');
+    require_once('html_footer.php');
+?>
---
+++ teste.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('html_header.php');
+require_once('header.php');
+require_once('footer.php');
+require_once('html_footer.php');
+
+
+?>
