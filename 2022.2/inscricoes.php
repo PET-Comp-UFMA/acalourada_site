263ff7bb0c8dc8f24062527ca9375d6c69f8e2fa
luscafp <lucasfariaslfp@gmail.com>
2023-02-07 11:04:32 -0300

    criação versão 2022.2

--- 2022.2/inscricoes.php
+++ 2022.2/inscricoes.php
@@ -19,13 +19,13 @@ require_once('header.php');
                         <ol style="padding-left: 0px">
                             <ol style="padding-left: 0px">
                                 <strong>
-                                    Inscreva-se no <a href="" target="blank">Formulário de
+                                    Inscreva-se no <a href="https://forms.gle/kPkpexCb9FqJ3mgC9" target="blank">Formulário de
                                         Inscrição</a> a para participar do evento.
                                 </strong>
                             </ol>
                             <ol style="padding-left: 0px">
                                 <strong>
-                                    Acesse o grupo do <a href="">Whatsapp</a>
+                                    Acesse o grupo do <a href="https://chat.whatsapp.com/DzIr8O9QMhdBESq6OIr1n1">Whatsapp</a>
                                     para ficar por dentro de tudo ou fale conosco pelo email.
                                 </strong>
                             </ol>
@@ -34,7 +34,7 @@ require_once('header.php');
                 </div>
 
                 <br>
-                <button class="faq-whatsapp btn"><a class="btn-subscription-text" href="" target="blank">Inscreva-se</a></button>
+                <button class="faq-whatsapp btn"><a class="btn-subscription-text" href="https://forms.gle/kPkpexCb9FqJ3mgC9" target="blank">Inscreva-se</a></button>
                 <br><br>
             </div>
         </div>
