899704470a8aa52e035a401f6a532c751c360f10
Vitor Gabriel <vitor.grs2004@gmail.com>
2024-09-09 09:46:12 -0300

    mudanças 2024.1

---
+++ 2021.1/inscricoes.php
@@ -0,0 +1,75 @@
+<?php
+    require_once('html_header.php');
+    require_once('header.php');
+?>
+    <main id="main">
+
+        <section id=inscricao>
+            <div class="container">
+                <div class="section-header">
+                    <h2>INSCRIÇÕES</h2>
+                </div>
+
+                <div class="text-center">
+                    <div class="row justify-content-center">
+                    <div class="col-lg-9">
+
+                        <!--<p>Orientações para participação da Acalourada Digital</p>-->
+
+                        <ol>
+                            <ol>
+                              <strong>
+                                Inscreva-se no Formulário de Inscrição</a> a para participar do evento.
+                              </strong>
+                            </ol>
+                            <ol>
+                              <strong>
+                                Acesse o grupo do Whatsapp</a> para ficar por dentro de tudo ou fale conosco pelo email.
+                              </strong>
+                            </ol>
+                            <ol>
+                              <strong>
+                                A sessão de jogos será realizada através do Discord</a> da Acalourada.
+                              </strong>
+                          </ol>
+                        </ol>
+                    </div>
+                </div>
+
+                    <br>
+                    <button class="faq-whatsapp btn"><a class="btn-subscription-text">Inscreva-se</a></button>
+                    <br><br>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <!--==========================
+        Footer
+    ============================
+
+    <footer id="footer" class="fixar-rodape">
+        <div class="footer-top">
+            <div class="footer-info" id="contatos">
+                <h4 >Contatos</h4>
+                <div id ="email">
+                    <strong>E-mail:</strong> petcomputacao.ufma@gmail.com
+                </div>
+                <div class="social-links">
+                    <a href="https://twitter.com/petcompufma" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="https://www.instagram.com/petcompufma/" class="instagram"><i class="fa fa-instagram"></i></a>
+                </div>  
+            </div>
+        </div>
+
+        <div class="copyright">
+            <strong>Acalourada 2021.1 - PETCOMP</strong>
+        </div>
+    </footer>
+    </div>-->
+
+<?php 
+    require_once('footer.php');
+    require_once('html_footer.php');
+?>
---
+++ 2021.2/inscricoes.php
@@ -0,0 +1,50 @@
+<?php
+  require_once('html_header.php');
+  require_once('header.php');
+?>
+        <main id="main">
+
+            <section id=inscricao>
+                <div class="container">
+                    <div class="section-header">
+                        <h2>INSCRIÇÕES</h2>
+                    </div>
+
+                    <div class="text-center">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-9">
+
+                                <!--<p>Orientações para participação da Acalourada Digital</p>-->
+
+                                <ol style="padding-left: 0px">
+                                    <ol style="padding-left: 0px">
+                                        <strong>
+                                            Inscreva-se no <a>Formulário de
+                                                Inscrição</a> a para participar do evento.
+                                        </strong>
+                                    </ol>
+                                    <ol style="padding-left: 0px">
+                                        <strong>
+                                            Acesse o grupo do <a
+                                               >Whatsapp</a>
+                                            para ficar por dentro de tudo ou fale conosco pelo email.
+                                        </strong>
+                                    </ol>
+                                </ol>
+                            </div>
+                        </div>
+
+                        <br>
+                        <button class="faq-whatsapp btn"><a class="btn-subscription-text"
+                                >Inscreva-se</a></button>
+                        <br><br>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+<?php 
+    require_once('footer.php');
+    require_once('html_footer.php');
+?>
