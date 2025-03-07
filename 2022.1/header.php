6e5af38d8704edbd38c51b25d80fd4e90b16ae21
oficialjoseguilherme <oficialjoseguilherme@gmail.com>
2025-03-07 11:44:42 -0300

    UPG Dropdown de versões: portabilidade do dropdown
    Desenvolvimento de uma solução para gerar mais portabilidade para o dropdown de versõespresente em todas as versões do site. A solução consiste na utilização de um arquivo global de dropdown que será inserido em cada versão do site no campo do dropdown, mitigando a necessidade de editar vários arquivos para a edição de todos os dropdowns. Unificando tudo em um arquivo só.

--- 2022.1/header.php
+++ 2022.1/header.php
@@ -51,28 +51,7 @@
                     <li class="dropdown">
                         <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown"
                             role="button" aria-haspopup="true" aria-expanded="false">Versões</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/acalourada/index.php">2025.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2024.2/index.php">2024.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourad/2024.1/index.php">2024.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2023.2/index.php">2023.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2023.1/index.php">2023.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2022.2/index.php">2022.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2022.1/index.php">2022.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2021.2/index.php">2021.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2021.1/index.php">2021.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="/acalourada/2020.2/index.php">2020.2</a>
-                        </div>
-                        </a>
+                        <?php include '../dropdown_versoes.php'; ?>
                     </li>
                 </ul>
             </nav> 
---
+++ dropdown_versoes.php
@@ -0,0 +1,21 @@
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" href="../index.php">2025.1</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2024.2/index.php">2024.2</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2024.1/index.php">2024.1</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2023.2/index.php">2023.2</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2023.1/index.php">2023.1</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2022.2/index.php">2022.2</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2022.1/index.php">2022.1</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2021.2/index.php">2021.2</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2021.1/index.php">2021.1</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="../2020.2/index.php">2020.2</a>
+</div>
