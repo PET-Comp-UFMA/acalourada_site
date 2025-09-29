42042618cdf2e1a12a195e2772879e4b1b4d339b
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2025-09-29 09:55:24 -0300

    Ultimas atualizações
    Versão definitiva pre acalourada

--- programacao.php
+++ programacao.php
@@ -28,7 +28,7 @@ require_once('header.php');
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Boas Vindas + Apresentação PETComp</h4>
-                        <p>Apresentador(a): Equipe PETComp<br>Local: A definir</p>
+                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
@@ -37,7 +37,7 @@ require_once('header.php');
                     <img src="img/speakers/prof.luis.png" alt="">
                     <div class="sub-infos">
                         <h4>Palestra sobre SBC e grupo Dexters</h4>
-                        <p>Apresentador(a): Prof. Luis Rivero<br>Local: A definir</p>
+                        <p>Apresentador(a): Prof. Luis Rivero<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
@@ -46,36 +46,36 @@ require_once('header.php');
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Dinâmica de Jogos</h4>
-                        <p>Apresentador(a): Equipe PETComp<br>Local: A definir</p>
+                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
                 <h3 class="schedule-day-title">TARDE</h3>
 
                 <div class="infos">
-                    <time>14:00-15:00</time>
+                    <time>16:00-16:30</time>
                     <img src="img/speakers/labi.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Liga de Bioinformática</h4>
-                        <p>Apresentador(a): LABI<br>Local: A definirT</p>
+                        <p>Apresentador(a): LABI<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>15:00-16:00</time>
+                    <time>16:30-17:30</time>
                     <img src="img/speakers/daComp.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação DAComp</h4>
-                        <p>Apresentadores: Equipe DAComp<br>Local: A definir</p>
+                        <p>Apresentadores: Equipe DAComp<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>16:00-16:30</time>
+                    <time>17:30-18:00</time>
                     <img src="img/speakers/lorde.png" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Atlética</h4>
-                        <p>Apresentador(a): Equipe da Atlética <br>Local: A definir</p>
+                        <p>Apresentador(a): Equipe da Atlética <br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
@@ -89,7 +89,7 @@ require_once('header.php');
                     <img src="img/speakers/DEINF.png" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação do Curso</h4>
-                        <p>Apresentador(a): Coordenação<br>Local: A definir</p>
+                        <p>Apresentador(a): Coordenação<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
@@ -98,7 +98,7 @@ require_once('header.php');
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Minicurso Lógica de Programação com Premiação</h4>
-                        <p>Apresentador(a):Petianos José Guilherme e Breno <br>Local: A definir</p>
+                        <p>Apresentador(a):Petianos Breno e Yan <br>Local:Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
@@ -107,19 +107,19 @@ require_once('header.php');
                 
                 <div class="infos">
                     <time>14:00-16:00</time>
-                    <img src="img/speakers/nca.jpeg" alt="">
+                    <img src="img/speakers/simara.png" alt="">
                     <div class="sub-infos">
-                        <h4>Apresentação NCA e Palestra</h4>
-                        <p>Apresentador(a): Prof. Geraldo Braz<br>Local: A definir</p>
+                        <h4>Palestra Mulheres na Computação</h4>
+                        <p>Apresentador(a): Profa. Simara Vieira<br>Local: Auditório do NCA</p>
                     </div>
                 </div>
 
                 <div class="infos">
                     <time>16:00-17:30</time>
-                    <img src="img/speakers/simara.png" alt="">
+                    <img src="img/speakers/nca.jpeg" alt="">
                     <div class="sub-infos">
-                        <h4>Palestra Mulheres na Computação</h4>
-                        <p>Apresentador(a): Profa. Simara Vieira<br>Local: A definir</p>
+                        <h4>apresentação NCA e Palestra</h4>
+                        <p>Apresentador(a): Prof. Anselmo Paiva<br>Local: Auditório do NCA</p>
                     </div>
                 </div>
 
@@ -128,7 +128,7 @@ require_once('header.php');
                  <h2 class="schedule-day-title">Dia 26/09</h2>
                 <h3 class="schedule-day-title">MANHÃ</h3>
                
-                
+                <!--
                 <div class="infos">
                     <time>08:40-09:10</time>
                     <img src="img/labs/lsdi.png" alt="">
@@ -136,14 +136,14 @@ require_once('header.php');
                         <h4>LSDI</h4>
                         <p>Apresentador: Francisco<br>Local: Prédio da Pós</p>
                     </div>
-                </div>
+                </div>-->
 
                 <div class="infos">
                     <time>09:20-09:50</time>
-                    <img src="img/labs/lacmor.png" alt="">
+                    <img src="img/labs/viplab.png" alt="">
                     <div class="sub-infos">
-                        <h4>LACMOR</h4>
-                        <p>Apresentador: Alexandre<br>Local: Prédio da Pós</p>
+                        <h4>VipLab (DEINF)</h4>
+                        <p>Apresentador: Geraldo Braz<br>Local: Prédio da Pós</p>
                     </div>
                 </div>
 
@@ -174,62 +174,46 @@ require_once('header.php');
                     </div>
                 </div>
 
-                <div class="infos">
-                    <time>12:00-12:30</time>
-                    <img src="img/labs/inovtec.png" alt="">
-                    <div class="sub-infos">
-                        <h4>Inovtec</h4>
-                        <p>Apresentador: Aroelino<br>Local: Prédio da Pós</p>
-                    </div>
-                </div>
-
 
                 <h3 class="schedule-day-title">TARDE</h3>
 
                 <div class="infos">
-                    <time>16:00-16:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
-                    <div class="sub-infos">
-                        <h4>Mesa Redonda com os egressos</h4>
-                        <p>Apresentador: Equipe PETComp<br>Local: CCET</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>16:00-16:30</time>
+                    <time>14:00-14:30</time>
                     <img src="img/labs/lint.png" alt="">
                     <div class="sub-infos">
                         <h4>LINT</h4>
-                        <p>Apresentador: Bonini<br>Local: CCET</p>
+                        <p>Apresentador: Tiago Bonini<br>Local: CCET</p>
                     </div>
                 </div>
-
+                <!--
                 <div class="infos">
-                    <time>16:40-17:10</time>
-                    <img src="img/labs/viplab.png" alt="">
-                    <div class="sub-infos">
-                        <h4>VipLab (DEINF)</h4>
-                        <p>Apresentador: Geraldo<br>Local: CCET</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>17:20-17:50</time>
+                    <time>14:40-15:10</time>
                     <img src="img/labs/modal.png" alt="">
                     <div class="sub-infos">
                         <h4>Modal</h4>
-                        <p>Apresentador: Glaubos<br>Local: CCET</p>
+                        <p>Apresentador: Francisco Glaubos<br>Local: CCET</p>
+                    </div>
+                </div>-->
+
+                <div class="infos">
+                    <time>16:00-18:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Mesa redonda com egressos</h4>
+                        <p>Apresentador: Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
 
-                <div class="infos">
+                 <div class="infos">
                     <time>18:00 - 18:30</time>
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Encerramento e Premiação</h4>
-                        <p>Apresentador: Equipe PETComp<br>Local: CCET</p>
+                        <p>Apresentador: Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
                     </div>
                 </div>
+
+               
 
             </div>
         </div>
