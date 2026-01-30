c9ce45542ac305cd592e1613822b82e6948cc056
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2026-01-30 11:55:40 -0300

    Atualizando programação

--- programacao.php
+++ programacao.php
@@ -11,94 +11,102 @@ require_once('header.php');
 <section id="schedule" class="section-with-bg">
     <div class="container wow fadeInUp">
         <div class="section-header">
-            <h2>Em breve, nossa programação estará disponível. Fique de olho nas nossas redes sociais.</h2>
+            <h2>PROGRAMAÇÃO DO EVENTO</h2>
         </div>
 
-        <!--  <h3 class="sub-heading">Organização - PETComp</h3> (Não Retirar)-->
+        <!--  <h3 class="sub-heading">Organização - PETComp</h3>-->
 
         <div class="tab-content row justify-content-center">
 
             <!-- Schdule Day 1 -->
-            <!--<div role="tabpanel" class="col-lg-9 tab-pane fade show active" id="day-1">
-                <h2 class="schedule-day-title">Dia 24/09</h2>
+            <div role="tabpanel" class="col-lg-9 tab-pane fade show active" id="day-1">
+                <h2 class="schedule-day-title">Dia XX/03</h2>
                 <h3 class="schedule-day-title">MANHÃ</h3>
                 
                 <div class="infos">
-                    <time>09:00-10:00</time>
+                    <time>08:30-10:30</time>
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Boas Vindas + Apresentação PETComp</h4>
-                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
+                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>10:00-11:00</time>
+                    <time>10:30-12:00</time>
                     <img src="img/speakers/prof.luis.png" alt="">
                     <div class="sub-infos">
                         <h4>Palestra sobre SBC e grupo Dexters</h4>
-                        <p>Apresentador(a): Prof. Luis Rivero<br>Local: Auditório 1 (CCET)</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>11:00-12:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
-                    <div class="sub-infos">
-                        <h4>Dinâmica de Jogos</h4>
-                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
+                        <p>Apresentador(a): Prof. Luis Rivero<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <h3 class="schedule-day-title">TARDE</h3>
 
                 <div class="infos">
-                    <time>16:00-16:30</time>
+                    <time>14:00-14:30</time>
                     <img src="img/speakers/labi.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Liga de Bioinformática</h4>
-                        <p>Apresentador(a): LABI<br>Local: Auditório 1 (CCET)</p>
+                        <p>Apresentador(a): LABI<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>16:30-17:30</time>
+                    <time>14:40-15:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação da LAJD (Liga de Jogos Digitais)</h4>
+                        <p>Apresentador(a): A definir<br>Local: Auditório da Pós (CCET)</p>
+                    </div>
+                </div>
+
+                <div class="infos">
+                    <time>15:00-16:00</time>
                     <img src="img/speakers/daComp.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação DAComp</h4>
-                        <p>Apresentadores: Equipe DAComp<br>Local: Auditório 1 (CCET)</p>
+                        <p>Apresentadores: Equipe DAComp<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>17:30-18:00</time>
+                    <time>16:00-16:30</time>
                     <img src="img/speakers/lorde.png" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Atlética</h4>
-                        <p>Apresentador(a): Equipe da Atlética <br>Local: Auditório 1 (CCET)</p>
+                        <p>Apresentador(a): Equipe da Atlética <br>Local: Auditório da Pós (CCET)</p>
                     </div>
-                </div>-->
+                </div>
 
                  <!-- Schdule Day 2 -->
 
-                <!--<h2 class="schedule-day-title">Dia 25/09</h2>
+                <h2 class="schedule-day-title">Dia XX/03</h2>
                 <h3 class="schedule-day-title">MANHÃ</h3>
 
                 <div class="infos">
                     <time>09:00-10:00</time>
-                    <img src="img/speakers/DEINF.png" alt="">
+                    <img src="img/logoCocom2.png" alt="">
                     <div class="sub-infos">
-                        <h4>Apresentação do Curso</h4>
-                        <p>Apresentador(a): Coordenação<br>Local: Auditório 1 (CCET)</p>
+                        <h4>Apresentação da Coordenação do Curso CP</h4>
+                        <p>Apresentador(a): Coordenação<br>Local: Auditório da Pós (CCET)</p>
+                    </div>
+                </div>
+                <div class="infos">
+                    <time>10:00-11:00</time>
+                    <img src="img/logoCocom2.png" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação da Coordenação do Curso IA</h4>
+                        <p>Apresentador(a): Coordenação<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>10:00-12:00</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <time>11:00-12:00</time>
+                    <img src="img/speakers/simara.png" alt="">
                     <div class="sub-infos">
-                        <h4>Minicurso Lógica de Programação com Premiação</h4>
-                        <p>Apresentador(a):Petianos Breno e Yan <br>Local:Auditório 1 (CCET)</p>
+                        <h4>Palestra</h4>
+                        <p>Apresentadora:Profa. Simara Vieira <br>Local:Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
@@ -107,114 +115,57 @@ require_once('header.php');
                 
                 <div class="infos">
                     <time>14:00-16:00</time>
-                    <img src="img/speakers/simara.png" alt="">
-                    <div class="sub-infos">
-                        <h4>Palestra Mulheres na Computação</h4>
-                        <p>Apresentador(a): Profa. Simara Vieira<br>Local: Auditório do NCA</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>16:00-17:30</time>
                     <img src="img/speakers/nca.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>apresentação NCA e Palestra</h4>
-                        <p>Apresentador(a): Prof. Anselmo Paiva<br>Local: Auditório do NCA</p>
-                    </div>
-                </div>-->
-
-
-                 <!-- Schdule Day 3  -->
-                <!-- <h2 class="schedule-day-title">Dia 26/09</h2>
-                <h3 class="schedule-day-title">MANHÃ</h3>-->
-               
-                <!--
-                <div class="infos">
-                    <time>08:40-09:10</time>
-                    <img src="img/labs/lsdi.png" alt="">
-                    <div class="sub-infos">
-                        <h4>LSDI</h4>
-                        <p>Apresentador: Francisco<br>Local: Prédio da Pós</p>
-                    </div>
-                </div>-->
-
-                <!--<div class="infos">
-                    <time>09:20-09:50</time>
-                    <img src="img/labs/viplab.png" alt="">
-                    <div class="sub-infos">
-                        <h4>VipLab (DEINF)</h4>
-                        <p>Apresentador: Geraldo Braz<br>Local: Prédio da Pós</p>
+                        <p>Apresentador(a): Prof. Anselmo Paiva<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
                 <div class="infos">
-                    <time>10:00-10:30</time>
-                    <img src="img/labs/laws.png" alt="">
-                    <div class="sub-infos">
-                        <h4>LAWS</h4>
-                        <p>Apresentador: Mário<br>Local: Prédio da Pós</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>10:40-11:10</time>
-                    <img src="img/labs/telemidia.png" alt="">
-                    <div class="sub-infos">
-                        <h4>Telemídia</h4>
-                        <p>Apresentador: Salles<br>Local: Prédio da Pós</p>
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <time>11:20-11:50</time>
-                    <img src="img/labs/viplab.png" alt="">
-                    <div class="sub-infos">
-                        <h4>VipLab (Pós)</h4>
-                        <p>Apresentador: Dallyson<br>Local: Prédio da Pós</p>
-                    </div>
-                </div>
-
-
-                <h3 class="schedule-day-title">TARDE</h3>
-
-                <div class="infos">
-                    <time>14:00-14:30</time>
-                    <img src="img/labs/lint.png" alt="">
-                    <div class="sub-infos">
-                        <h4>LINT</h4>
-                        <p>Apresentador: Tiago Bonini<br>Local: CCET</p>
-                    </div>
-                </div>-->
-                <!--
-                <div class="infos">
-                    <time>14:40-15:10</time>
-                    <img src="img/labs/modal.png" alt="">
-                    <div class="sub-infos">
-                        <h4>Modal</h4>
-                        <p>Apresentador: Francisco Glaubos<br>Local: CCET</p>
-                    </div>
-                </div>-->
-
-                <!--<div class="infos">
                     <time>16:00-18:00</time>
                     <img src="img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
-                        <h4>Mesa redonda com egressos</h4>
-                        <p>Apresentador: Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
+                        <h4>Mesa Redonda com Egressos</h4>
+                        <p>Apresentador(a): Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
                     </div>
                 </div>
 
-                 <div class="infos">
-                    <time>18:00 - 18:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
-                    <div class="sub-infos">
-                        <h4>Encerramento e Premiação</h4>
-                        <p>Apresentador: Equipe PETComp<br>Local: Auditório 1 (CCET)</p>
-                    </div>
-                </div>-->
 
+                 <!-- Schdule Day 3  -->
+                 <h2 class="schedule-day-title">Dia XX/03</h2>
+                <h3 class="schedule-day-title">MANHÃ</h3>
                
 
+                <div class="infos">
+                    <time>08:30-13:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação dos Laboratórios</h4>
+                        <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
+                    </div>
+                </div>
+
+
+
+                <h3 class="schedule-day-title">TARDE</h3>
+                
+                <div class="infos">
+                    <time>14:00-16:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação dos Laboratórios</h4>
+                        <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
+                    </div>
+                </div>
+                <div class="infos">
+                    <time>16:00-17:30</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Dinâmica de Jogos + Encerramento</h4>
+                        <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
