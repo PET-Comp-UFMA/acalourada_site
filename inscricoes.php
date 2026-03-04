f899f9012d35a9d0e37dd39c34806a9edf339885
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2026-03-04 11:47:36 -0300

    Atualizando Design da página de inscrição

--- inscricoes.php
+++ inscricoes.php
@@ -11,7 +11,7 @@ require_once('header.php');
         <div class="section-header">
             <h2>INSCRIÇÕES</h2>
         </div>
-         <div class="d-flex justify-content-center align-items-center text-center" style="height:60vh;">
+         <div class="d-flex justify-content-center align-items-center text-center">
             <div class="row justify-content-center">
                 <div class="col-lg-9">
                     <ol>
