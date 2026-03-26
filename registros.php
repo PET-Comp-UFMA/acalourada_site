6a17bbdb70a33e25adf239ca88728245602189d9
Roberth <roberth.diniz@discente.ufma.br>
2026-03-26 11:43:34 -0300

    otimizando pagina registros

--- registros.php
+++ registros.php
@@ -9,94 +9,39 @@
     <div class="swiper carousel">
         <div class="swiper-wrapper">
             
+            <?php
+            // array pra guardar/adicionar imagens
+            $fotos = [
+                "Dia1 (1).jpg",
+                "Dia1 (2).jpg",
+                "Dia1 (3).jpg",
+                "Dia1 (4).jpg",
+                "Dia1 (5).jpg",
+                "Dia1 (6).jpg"
+            ];
+
+            // laço automatico
+            foreach ($fotos as $foto) {
+                echo '
+                <div class="swiper-slide">
+                    <div class="container-registros">
+                        <img src="img/registros/2026.1/dia1/'.$foto.'" alt="">
+                    </div>
+                </div>';
+            }
+            ?>
+            <!-- VERSÃO ANTIGA (manual)
+
             <div class="swiper-slide"> 
                 <img  src="img/registros/2026.1/dia 1/Dia_1 (1).jpg" alt="">
             </div>
-            <div class="swiper-slide"> 
-                <img  src="img/registros/2026.1/dia 1/Dia_1 (2).jpg" alt="">
-            </div>
-            <div class="swiper-slide"> 
-                <img  src="img/registros/2026.1/dia 1/Dia_1 (3).jpg" alt="">
-            </div>
-            <div class="swiper-slide"> 
-                <img  src="img/registros/2026.1/dia 1/Dia_1 (4).jpg" alt="">
-            </div>
-            <div class="swiper-slide"> 
-                <img  src="img/registros/2026.1/dia 1/Dia_1 (5).jpg" alt="">
-            </div>
-            <div class="swiper-slide"> 
-                <img  src="img/registros/2026.1/dia 1/Dia_1 (6).jpg" alt="">
-            </div>
-            <!--
-            <div class="swiper-slide">
-                <img src="img/registros/2025.2/dia 1/Foto dia 1 (2).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="img/registros/2025.2/dia 1/Foto dia 1 (3).jpg" alt="">
-            </div>
+
             <div class="swiper-slide">
                 <div class="container-registros">
                     <img src="img/registros/2025.2/dia 1/Foto dia 1 (4).jpg" alt="">
                 </div>
             </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Conspeq (4).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Connection.jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 1/Atletica (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 1/Atletica (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos.jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (1).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 1/Jogos (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 1/Jogos (3).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 1/Jogos (4).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (5).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (6).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (7).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (8).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (9).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (10).jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="./img/registros/2023.1/Dia 1/Jogos (11).jpg" alt="">
-            </div>
+
             -->
         </div>
         <button type="button" class="swiper-button-next"></button>
@@ -110,56 +55,38 @@
     <div class="swiper carousel">
         <div class="swiper-wrapper">
 
+            <?php
+            // array pra guardar/adicionar imagens
+            $fotos = [
+                "Dia2 (1).jpg",
+                "Dia2 (2).jpg",
+                "Dia2 (3).jpg",
+                "Dia2 (4).jpg"
+            ];
+
+            // laço automatico
+            foreach ($fotos as $foto) {
+                echo '
+                <div class="swiper-slide">
+                    <div class="container-registros">
+                        <img src="img/registros/2026.1/dia2/'.$foto.'" alt="">
+                    </div>
+                </div>';
+            }
+            ?>
+            <!-- VERSÃO ANTIGA (manual)
+
             <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 2/Dia 2 (1).jpg" alt="">
-                </div>
+                <img  src="img/registros/2026.1/dia 1/Dia_1 (1).jpg" alt="">
             </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 2/Dia 2 (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 2/Dia 2 (3).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 2/Dia 2 (4).jpg" alt="">
-                </div>
-            </div>
-            <!-- <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 2/foto dia 2 (2).jpg" alt="">
-                </div>
-            </div>
+
             <div class="swiper-slide">
                 <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 2/foto dia 2 (3).jpg" alt="">
+                    <img src="img/registros/2025.2/dia 1/Foto dia 1 (4).jpg" alt="">
                 </div>
             </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 2/Palestra (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 2/Palestra (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 2/SBC (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 2/SBC (2).jpg" alt="">
-                </div>
-            </div> -->
+            
+            -->
         </div>
         <button type="button" class="swiper-button-next"></button>
         <button type="button" class="swiper-button-prev"></button>
@@ -172,136 +99,26 @@
     <div class="swiper carousel">
         <div class="swiper-wrapper">
 
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 3/Dia 3 (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 3/Dia 3 (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 3/Dia 3 (3).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 3/Dia 3 (4).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2026.1/dia 3/Dia 3 (5).jpg" alt="">
-                </div>
-            </div>
-            <!-- <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 3/fotos dia 3 (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 3/fotos dia 3 (3).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 3/fotos dia 3 (4).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 3/fotos dia 3 (5).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="img/registros/2025.2/dia 3/fotos dia 3 (6).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (3).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (4).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (5).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (6).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (7).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (8).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (9).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (10).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (11).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (12).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Labtour (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/PETComp (1).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/PETComp (2).jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Premiacao.jpg" alt="">
-                </div>
-            </div>
-            <div class="swiper-slide"> 
-                <div class="container-registros">
-                    <img class="container-registros" src="./img/registros/2023.1/Dia 3/Encerramento.jpg" alt="">
-                </div>
-            </div> -->
+            <?php
+            // array pra guardar/adicionar imagens
+            $fotos = [
+                "Dia3 (1).jpg",
+                "Dia3 (2).jpg",
+                "Dia3 (3).jpg",
+                "Dia3 (4).jpg",
+                "Dia3 (5).jpg"
+            ];
+
+            // laço automatico
+            foreach ($fotos as $foto) {
+                echo '
+                <div class="swiper-slide">
+                    <div class="container-registros">
+                        <img src="img/registros/2026.1/dia3/'.$foto.'" alt="">
+                    </div>
+                </div>';
+            }
+            ?>
        </div>
         <button type="button" class="swiper-button-next"></button>
         <button type="button" class="swiper-button-prev"></button>
