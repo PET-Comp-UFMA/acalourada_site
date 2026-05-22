381dcc3edf8853ea077fc68cc8ecedcddc4f0461
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2026-05-22 11:51:40 -0300

    Novo header e atualizando caminhos

--- 2026.1/programacao.php
+++ 2026.1/programacao.php
@@ -25,7 +25,7 @@ require_once('header.php');
                 
                 <div class="infos">
                     <time>08:30-10:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Boas Vindas + Apresentação PETComp</h4>
                         <p>Apresentador(a): Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
@@ -34,7 +34,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>10:30-12:00</time>
-                    <img src="img/speakers/prof.luis.png" alt="">
+                    <img src="../img/speakers/prof.luis.png" alt="">
                     <div class="sub-infos">
                         <h4>Palestra sobre SBC e grupo Dexters</h4>
                         <p>Apresentador(a): Prof. Luis Rivero<br>Local: Auditório da Pós (CCET)</p>
@@ -45,7 +45,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>14:00-14:30</time>
-                    <img src="img/speakers/labi.jpeg" alt="">
+                    <img src="../img/speakers/labi.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Liga de Bioinformática</h4>
                         <p>Apresentador(a): LABI<br>Local: Auditório da Pós (CCET)</p>
@@ -54,7 +54,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>14:40-15:00</time>
-                    <img src="img/LAJD.jpeg" alt="">
+                    <img src="../img/LAJD.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da LAJD (Liga de Jogos Digitais)</h4>
                         <p>Apresentador(a): A definir<br>Local: Auditório da Pós (CCET)</p>
@@ -63,7 +63,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>15:00-16:00</time>
-                    <img src="img/speakers/daComp.jpeg" alt="">
+                    <img src="../img/speakers/daComp.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação DAComp</h4>
                         <p>Apresentadores: Equipe DAComp<br>Local: Auditório da Pós (CCET)</p>
@@ -72,7 +72,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>16:00-17:00</time>
-                    <img src="img/speakers/lorde.png" alt="">
+                    <img src="../img/speakers/lorde.png" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação da Atlética</h4>
                         <p>Apresentador(a): Equipe da Atlética <br>Local: Auditório da Pós (CCET)</p>
@@ -86,7 +86,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>08:30-10:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação das Coordenações</h4>
                         <p>Apresentador(a): Coordenação<br>Local: Auditório da Pós (CCET)</p>
@@ -95,7 +95,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>10:30-12:00</time>
-                    <img src="img/speakers/simara.png" alt="">
+                    <img src="../img/speakers/simara.png" alt="">
                     <div class="sub-infos">
                         <h4>Palestra</h4>
                         <p>Apresentadora:Profa.Dra. Simara Vieira <br>Local:Auditório da Pós (CCET)</p>
@@ -107,7 +107,7 @@ require_once('header.php');
                 
                 <div class="infos">
                     <time>14:00-15:30</time>
-                    <img src="img/speakers/nca.jpeg" alt="">
+                    <img src="../img/speakers/nca.jpeg" alt="">
                     <div class="sub-infos">
                         <h4>apresentação NCA e Palestra</h4>
                         <p>Apresentador(a): Prof.Dr. Anselmo Paiva<br>Local: Auditório da Pós (CCET)</p>
@@ -116,7 +116,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>15:30-17:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Mesa Redonda com Egressos</h4>
                         <p>Apresentador(a): Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
@@ -131,7 +131,7 @@ require_once('header.php');
 
                 <div class="infos">
                     <time>08:30-12:00</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação dos Laboratórios</h4>
                         <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
@@ -144,7 +144,7 @@ require_once('header.php');
                 
                 <div class="infos">
                     <time>14:00-15:30</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Apresentação dos Laboratórios</h4>
                         <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
@@ -152,7 +152,7 @@ require_once('header.php');
                 </div>
                 <div class="infos">
                     <time>15:30-18:00</time>
-                    <img src="img/speakers/pet.jpg" alt="">
+                    <img src="../img/speakers/pet.jpg" alt="">
                     <div class="sub-infos">
                         <h4>Dinâmica PETComp + Premiação</h4>
                         <p>Apresentador: Equipe PETComp<br>Local: Auditório da Pós (CCET)</p>
--- header.php
+++ header.php
@@ -1,61 +1,102 @@
-<div class="wrapper">
-    <div class="banner">
-        <div>
-            <a href="index.php"><img src="img/favicon-v1/icone-acalourada.svg" alt="" class="img-fluid" id="img-esq"></a>
-        </div>
-        <div>
-            <a href="index.php"><img src="img/bannerAcalourada2025_1.svg" class="img-fluid" id="img-centro"></a>
-        </div>
-        <div>
-            <a href="https://portalpadrao.ufma.br/site" target="_blank"><img src="img/supporters/logo-ufma.png" alt=""
-                    class="img-fluid" id="img-dir"></a>
-        </div>
+<link rel="stylesheet" href="css/Novo_CSS/header.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+
+<header class="site-header">
+
+    <div class="site-header__inner">
+
+        <!-- LOGO -->
+        <a href="index.php" class="site-brand">
+
+            <img
+                src="img/favicon-v1/IconV2.svg"
+                alt="Logo Acalourada"
+                class="site-brand__icon"
+            >
+
+            <img
+                src="img/favicon-v1/BannerV2.svg"
+                alt="Acalourada"
+                class="site-brand__text"
+            >
+
+        </a>
+
+        <!-- NAVBAR -->
+        <nav class="site-navbar">
+
+            <ul class="site-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">
+                        HOME
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="programacao.php">
+                        PROGRAMAÇÃO
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="localizacao.php">
+                        LOCALIZAÇÃO
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="labs.php">
+                        LABS
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="registros.php">
+                        REGISTROS
+                    </a>
+                </li>
+
+                <!-- DROPDOWN -->
+                <li class="nav-item dropdown">
+
+                    <a
+                        href="#"
+                        class="nav-link dropdown-toggle"
+                    >
+                        VERSÕES
+                    </a>
+
+                    <div class="site-dropdown">
+
+                        <a href="index.php">2026.2</a>
+                        <a href="2026.1/index.php">2026.1</a>
+                        <a href="2025.2/index.php">2025.2</a>
+                        <a href="2025.1/index.php">2025.1</a>
+                        <a href="2024.2/index.php">2024.2</a>
+                        <a href="2024.1/index.php">2024.1</a>
+                        <a href="2023.2/index.php">2023.2</a>
+                        <a href="2023.1/index.php">2023.1</a>
+                        <a href="2022.2/index.php">2022.2</a>
+                        <a href="2022.1/index.php">2022.1</a>
+                        <a href="2021.2/index.php">2021.2</a>
+                        <a href="2021.1/index.php">2021.1</a>
+                        <a href="2020.2/index.php">2020.2</a>
+
+                    </div>
+
+                </li>
+
+            </ul>
+
+      
+            <a href="inscricoes.php" class="btn-inscreva">
+                INSCREVA-SE
+            </a>
+
+        </nav>
+
     </div>
-    
-    <header id="header">
-        <div class="container">
-            <nav id="nav-menu-container">
-                <ul class="nav-menu">
-                    <li><a href="index.php">Home</a></li>
-                    <li class="menu"><a href="inscricoes.php">Inscrições</a></li>
-                    <li><a href="programacao.php">Programação</a></li>
-                    <li><a href="localizacao.php">Localização</a></li>
-                    <li><a href="">Labs</a></li>
-                    <li><a href="registros.php">Registros</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown"
-                            role="button" aria-haspopup="true" aria-expanded="false">Versões</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="index.php">2026.2</a>
-                                <div class="dropdown-divider" ></div>
-                                <a class="dropdown-item" href="2026.1/index.php">2026.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2025.2/index.php">2025.2</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2025.1/index.php">2025.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2024.2/index.php">2024.2</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2024.1/index.php">2024.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2023.2/index.php">2023.2</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2023.1/index.php">2023.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2022.2/index.php">2022.2</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2022.1/index.php">2022.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2021.2/index.php">2021.2</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2021.1/index.php">2021.1</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="2020.2/index.php">2020.2</a>
-                            </div>
-                    </li>
-                    <li><a href="">Inscreva-se</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-</div>
+
+</header>
