03ba166209402e52ea7d81a69e8e5c16c34471f0
Yan Felipe <yanfelipebp@gmail.com>
2026-05-22 12:06:37 -0300

    Inciando Footer

--- footer.php
+++ footer.php
@@ -1,18 +1,32 @@
-<footer id="footer" class="fixar-rodape">
-    <div class="footer-top">
-        <div class="footer-info" id="contatos">
-            <h4>Contatos</h4>
-            <div id="email">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css\Novo_CSS\footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet">
+</head>
 
-                <strong>E-mail:</strong> petcomputacao.ufma@gmail.com
-            </div>
-            <div class="social-links">
+<body>
+    <footer id="footer" class="fixar-rodape">
+        <div class="footer-top">
+            <div class="footer-info" id="contatos">
+                <img src="" alt="">
+                <h4>Contatos</h4>
+                <div id="email">
+                    <strong>E-mail:</strong> petcomputacao.ufma@gmail.com
+                </div>
+                <div class="social-links">
 
-                <a href="https://www.instagram.com/petcompufma/" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
+                    <a href="https://www.instagram.com/petcompufma/" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="copyright">
-        <strong>Acalourada 2026.1 - PETCOMP</strong>
-    </div>
-</footer>
+        <div class="copyright">
+            <strong>Acalourada 2026.2   •   Ciência da Computação e Inteligência Artificial   •   PETComp</strong>
+        </div>
+    </footer>
+</body>
