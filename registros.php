3c7692c15579013a3571d4d2c91eb8834c230791
Roberth <roberth.diniz@discente.ufma.br>
2026-05-28 23:31:45 -0300

    Adicionando botoes em registros

--- registros.php
+++ registros.php
@@ -5,11 +5,11 @@
 ?>
 
 <link rel="stylesheet" href="css/Novo_CSS/registros.css">
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
 <section class="hero-registros">
     <div class="hero-registros-content">
-        <h1>Registros Acalourada <span>2026.2</span></h1>
+        <h1>Registros Acalourada <span>2026.1</span></h1>
         <p>
             Os melhores cliques dos três dias do evento vivido
             pelos calouros do curso de Ciência da Computação
@@ -18,9 +18,17 @@
     </div>
 </section>
 
+<div class="dias-container">
+    <button class="btn-dia ativo">Dia 1</button>
+    <button class="btn-dia">Dia 2</button>
+    <button class="btn-dia">Dia 3</button>
+</div>
+
+<script src="js/registrosBotoes.js"></script>
+
 <section class="registros-banner">
     <img 
-        src="img\BannerRegistros.png" 
+        src="img/BannerRegistros.png" 
         alt="Banner registros"
         class="banner-registros-img"
     >
