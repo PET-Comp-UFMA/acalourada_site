f1e7855b86d005b586b3e0b1634b26f42740009b
Yan Felipe <yanfelipebp@gmail.com>
2026-06-02 12:06:50 -0300

    iniciando inscrições

--- inscricoes.php
+++ inscricoes.php
@@ -1,39 +1,79 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscrições</title>
+    <link rel="stylesheet" href="css\Novo_CSS\inscricoes.css">
+    <!-- Font Montserat -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet">
+    <!-- Font Karantina -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+    require_once('header.php');
+    ?>
+    <div class="inscricoes">
+        <div class="inscricoes-info">
 
-require_once('html_header.php');
+            <div class="info-text">
+                <h1>FAÇA PARTE DA</h1>
+                <h1 class="acalourada">ACALOURADA 2026.2 </h1>
 
-require_once('header.php');
-
-?>
-
-<main id="main">
-    <section id=inscricao>
-        <div class="section-header">
-            <h2>INSCRIÇÕES</h2>
-        </div>
-         <div class="d-flex justify-content-center align-items-center text-center">
-            <div class="row justify-content-center">
-                <div class="col-lg-9">
-                    <ol>
-                        <strong>
-                            Inscreva-se no <a href="https://docs.google.com/forms/d/1IJJCU9oDhMePUMxDs0vaUOI1XOPW7nPphDudKwipqVU/edit?pli=1" target="blank">Formulário de
-                            Inscrição</a> a para participar do evento.
-                            Acesse o grupo do <a href="https://chat.whatsapp.com/IuD54WnIoue7LeDpN47bNz?mode=gi_t">Whatsapp</a>
-                            para ficar por dentro de tudo ou fale conosco pelo email.
-                        </strong>
-                    </ol>
-              
+                <p>Preencha o formulário ao lado para garantir sua <br>participação na recepção dos calouros de <br>Ciência da Computação e Inteligência Artificial da UFMA</p>
             </div>
-           <!--<button class="btn-inscricao btn" onclick="window.open('https://forms.gle/RSmA5jorLQf4Mb6a9', '_blank')">Inscreva-se</button>-->
 
+            <div class="info-cards">
+                <div class= "card-vertical">
+
+                    <div class="card">
+                        <img src="img\inscricoes\calendario_icon.svg" alt="icon calendário">
+                        <h3>6 anos</h3>
+                        <p>de tradição</p>
+                    </div>
+
+                    <div class="card">
+                        <img src="img\inscricoes\icon_pessoas.svg" alt="icon pessoas">
+                        <h3>+400</h3>
+                        <p>calouros já participaram</p>
+                    </div>
+
+                    <div class="card">
+                        <img src="img\inscricoes\joia_icon.svg" alt="icon joia">
+                        <h3>XX%</h3>
+                        <p>de aprovação entre calouros</p>
+                    </div>
+                </div>
+
+                <div class="card-horizontal">
+
+                    <div class="card" id="horizontal">
+                        <img src="img\inscricoes\icon_pessoas.svg" alt="icon pessoas">
+
+                        <div class="info">
+                            <h3>Aprimoramento</h3>
+                            <p>Utilizaremos seus dados para organizar uma experiência melhor e mais personalizada no evento!</p>
+                        </div>
+
+                    </div>
+
+                </div>  
+            </div>
         </div>
-    </section>
-</main>
 
-<?php
+        <div class="inscricoes-form">
+            <div class="form"></div>
+        </div>
 
-require_once('footer.php');
-
-require_once('html_footer.php');
-
-?>
+    </div>
+    <?php
+    require_once('footer.php');
+    require_once('html_footer.php');
+    ?>  
+</body>
+</html>
