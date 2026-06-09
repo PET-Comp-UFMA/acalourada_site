80ca47c25f7c80be7ae261cbb131fcf4a28bd6c7
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2026-06-09 12:07:16 -0300

    Finalizando página index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    $cssExtra = "css/Novo_CSS/index.css";
-    require_once('header.php');
+$cssExtra = "css/Novo_CSS/index.css";
+require_once('header.php');
 ?>
 
 <section class="hero">
@@ -15,7 +15,7 @@
             <h1>
                 ACALOURADA DE
                 <span>CIÊNCIA DA COMPUTAÇÃO E
-                INTELIGÊNCIA ARTIFICIAL</span>
+                    INTELIGÊNCIA ARTIFICIAL</span>
                 PETCOMP • UFMA
             </h1>
 
@@ -30,11 +30,6 @@
             </a>
 
         </div>
-
-        <div class="hero-image">
-            <img src="img/imagens_novo_site/astronauta (1).png" alt="Astronauta">
-        </div>
-
     </div>
 
 </section>
@@ -44,19 +39,20 @@
     <div class="container">
 
         <div class="sobre-icon">
-            <img src="" alt="Foguete">
+            <img src="img/imagens_novo_site/icons/foguete.svg" alt="Foguete">
         </div>
 
         <div class="sobre-text">
 
             <h2>SOBRE O EVENTO</h2>
+            <div class="sobre-barra"></div>
 
             <p>
-                A Acalourada é uma atividade de recepção dos estudantes
-                ingressantes no curso de Ciência da Computação de forma
-                presencial. Essa atividade já vem sendo desenvolvida há
-                mais de 6 anos em conjunto com a Coordenação de Computação,
-                o Departamento de Informática e o PETComp.
+                A Acalourada é uma atividade de recepção dos estudantes ingressantes no curso de Ciência da
+                Computação de forma presencial. Essa atividade já vem sendo desenvolvida de forma presencial há mais de
+                6 anos em conjunto com a Coordenação de Computação, o Departamento de Informática e o PET de Computação,
+                e procura guiar os calouros através da universidade e do curso, além de mostrar as oportunidades que a
+                universidade oferece tanto de forma pessoal como profissional.
             </p>
 
         </div>
@@ -64,22 +60,22 @@
         <div class="sobre-cards">
 
             <div class="sobre-card">
-                <img src="" alt="">
+                <img src="img/imagens_novo_site/icons/conferencia.svg" alt="">
                 <span>Integração</span>
             </div>
 
             <div class="sobre-card">
-                <img src="" alt="">
+                <img src="img/imagens_novo_site/icons/inovacao.svg" alt="">
                 <span>Conhecimento</span>
             </div>
 
             <div class="sobre-card">
-                <img src="" alt="">
+                <img src="img/imagens_novo_site/icons/Star 1.svg" alt="">
                 <span>Experiência</span>
             </div>
 
             <div class="sobre-card">
-                <img src="" alt="">
+                <img src="img/imagens_novo_site/icons/Polygon 27.svg" alt="">
                 <span>Desenvolvimento</span>
             </div>
 
@@ -94,11 +90,12 @@
     <div class="container">
 
         <h2>ORGANIZAÇÃO</h2>
+        <div class="organizacao-barra"></div>
 
         <div class="organizacao-grid">
 
             <div class="organizacao-card">
-                <img src="" alt="UFMA">
+                <img src="img/supporters/logo-ufma.png" alt="UFMA">
 
                 <h3>UFMA</h3>
 
@@ -108,7 +105,7 @@
             </div>
 
             <div class="organizacao-card">
-                <img src="" alt="COCOM">
+                <img src="img/cocomSVG.svg" alt="COCOM">
 
                 <h3>COCOM</h3>
 
@@ -118,7 +115,7 @@
             </div>
 
             <div class="organizacao-card">
-                <img src="" alt="PETComp">
+                <img src="img/supporters/5.png" alt="PETComp">
 
                 <h3>PETComp</h3>
 
@@ -138,22 +135,23 @@
     <div class="container">
 
         <h2>FAQ</h2>
+        <div class="faq-barra"></div>
 
         <p class="faq-desc">
-            Aqui você encontra perguntas frequentes sobre o evento,
-            início das aulas e o curso.
+            Aqui você encontra perguntas frequentes que você pode chegar a ter sobre o evento, o início das aulas ou
+            simplesmente sobre o curso.Não conseguiu a resposta que queria? Envie sua dúvida por nossos meios de
+            comunicação no final dessa página.
         </p>
 
         <div class="faq-item">
 
             <button class="faq-question">
-                Como eu posso saber meus horários de aula?
+                Como eu posso saber meus horários de aula?<span class="seta">▼
             </button>
 
             <div class="faq-answer">
-                Para ter acesso aos horários de aula,
-                você precisa logar no SIGAA e acessar
-                a aba de ensino.
+                Para ter acesso aos seus horários de aula, você precisa logar no SIGAA e clicar na aba de ensino, depois
+                em atestado de matrícula.
             </div>
 
         </div>
@@ -161,12 +159,15 @@
         <div class="faq-item">
 
             <button class="faq-question">
-                Como funcionam as notas?
+                Como funcionam as notas?<span class="seta">▼
             </button>
 
             <div class="faq-answer">
-                As notas são divididas em três avaliações,
-                reposição e prova final.
+                As notas são divididas em três provas, uma prova de reposição e uma prova final que variam de 0 a 10. Se
+                a média das notas das três provas for menor que 7 é necessário fazer uma reposição da nota mais baixa,
+                se ainda assim a média das duas maiores notas com a reposição for menor que 7 então você deve fazer uma
+                prova final, assim a última forma de conseguir passar é conseguindo que a soma da média anterior com a
+                nota da prova final seja maior ou igual a 12.
             </div>
 
         </div>
@@ -174,11 +175,12 @@
         <div class="faq-item">
 
             <button class="faq-question">
-                Com quanto tempo consigo me formar?
+                Com quanto tempo consigo me formar?<span class="seta">▼
             </button>
 
             <div class="faq-answer">
-                O tempo previsto é de acordo com a matriz curricular.
+                A duração do curso é de 4 anos, mas você tem 2 anos a mais de prazo para concluir sua graduação. Não se
+                desespere nas disciplinas, faça tudo a seu tempo.
             </div>
 
         </div>
@@ -186,11 +188,41 @@
         <div class="faq-item">
 
             <button class="faq-question">
-                Como fico sabendo dos eventos de computação?
+                Como fico sabendo dos eventos de computação?<span class="seta">▼
             </button>
 
             <div class="faq-answer">
-                Através das redes sociais e grupos oficiais do curso.
+                Para se ter conhecimento acerca dos eventos, normalmente há divulgação nas salas de aula e ao longo do
+                prédio do CCET. Além disso, o PETComp também procura divulgar através de suas redes sociais e site. Os
+                eventos de computação também podem ser acessados pelo site da SBC - Sociedade Brasileira de Computação.
+            </div>
+
+        </div>
+
+        <div class="faq-item">
+
+            <button class="faq-question">
+                O que é o PETComp?<span class="seta">▼
+            </button>
+
+            <div class="faq-answer">
+                O PETComp é o Programa de Ensino Tutorial de Computação e tem como objetivo realizar diversas atividades
+                de pesquisa, ensino e extensão. Dentre algumas das atividades podemos citar minicursos, Projetos de
+                Extensão,como por exemplo, ensino de pensamento computacional, organizar o Encontro Acadêmico de
+                Computação (EAComp), desenvolvimento de sistemas, entre outros.
+            </div>
+
+        </div>
+
+
+        <div class="faq-item">
+
+            <button class="faq-question">
+                O que é CR (Coeficiente de Rendimento)?<span class="seta">▼
+            </button>
+
+            <div class="faq-answer">
+                O coeficiente de rendimento (CR) do aluno será obtido pela média ponderada das disciplinas cursadas com aproveitamento ou não, sendo os pesos representados pelos créditos das respectivas disciplinas, e como divisor a soma dos respectivos créditos.
             </div>
 
         </div>
@@ -201,19 +233,19 @@
 
 <script>
 
-document.querySelectorAll('.faq-question').forEach(button => {
+    document.querySelectorAll('.faq-question').forEach(button => {
 
-    button.addEventListener('click', () => {
+        button.addEventListener('click', () => {
 
-        button.parentElement.classList.toggle('active');
+            button.parentElement.classList.toggle('active');
+
+        });
 
     });
-
-});
 
 </script>
 
 <?php
-    require_once('footer.php');
-    require_once('html_footer.php');
+require_once('footer.php');
+require_once('html_footer.php');
 ?>
